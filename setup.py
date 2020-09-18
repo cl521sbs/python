e80f39a00dd0c37e7d19f9caaffcdb46ec205721
Wang <710008625@genpact.com>
2020-09-18 15:09:28 +0800

    yayayaya

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+from setuptools import find_packages, setup
 
-@app.route('/')
-def index():
-    return 'hello, world!'
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
